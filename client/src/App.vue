486fa3ddd68c28940c4fb45543f867b60ab2b873
Zalary Young <zalary@activeprospect.com>
2019-02-11 22:11:56 -0600

    initial commit

---
+++ client/src/App.vue
@@ -0,0 +1,9 @@
+<style lang="scss">
+@import 'src/assets/css/style.scss';
+</style>
+
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
---
+++ client/vue.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  devServer: {
+    port: 8080,
+    proxy: 'http://localhost:5500'
+  },
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.(graphql|gql)$/,
+          exclude: /node_modules/,
+          loader: 'graphql-tag/loader'
+        }
+      ]
+    }
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        data: `@import "@/assets/css/style.scss";`,
+        data: `@import "@/assets/css/variables.scss";`
+      }
+    }
+  }
+}
